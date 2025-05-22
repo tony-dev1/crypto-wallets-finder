e685b888443fb5f2e5b150b11b9ab524fd032f20
TonyDevBTC <micheline_farrier@hotmail.com>
2025-05-22 12:00:52 +0200

    Update addres_generator.cpp

--- src/addres_generator.cpp
+++ src/addres_generator.cpp
@@ -1,137 +1,151 @@
-#include "addres_generator.h"
+/* 
+* This file is part of the Trezor Crypto library.
+* 
+* Copyright (c) 2021 SatoshiLabs s.r.o. (https://www.satoshilabs.com)
+* 
+* This code is licensed under the MIT License (MIT), see LICENSE file for details.
+*/
 
-BIP32Node derive_master_key(const std::vector<uint8_t>& seed)
-{
-    unsigned int len = 64;
-    std::vector<unsigned char> I(len);
 
-    HMAC(EVP_sha512(),
-        "Bitcoin seed", strlen("Bitcoin seed"),
-        seed.data(), seed.size(),
-        I.data(), &len);
+#include "address_generator.h"
+#include <sstream>
+#include <iomanip>
+#include <string.h>
 
-    BIP32Node node;
-    node.privkey = std::vector<unsigned char>(I.begin(), I.begin() + 32);
-    node.chain_code = std::vector<unsigned char>(I.begin() + 32, I.end());
+extern "C" {
 
-    node.pubkey = private_to_public_key(node.privkey);
-    return node;
-}
+    void mnemonic_to_seed(const char *mnemonic, const char *passphrase,
+        uint8_t seed[512 / 8],
+        void (*progress_callback)(uint32_t current,
+                                  uint32_t total));
 
-BIP32Node derive_key(const BIP32Node& parent_node, const std::string& path)
-{
-    std::vector<unsigned char> privkey = parent_node.privkey;
-    std::vector<unsigned char> chain_code = parent_node.chain_code;
+    int hdnode_from_seed(const uint8_t *seed, int seed_len, const char *curve,
+                                    HDNode *out);
 
-    size_t start = 0;
-    while (start < path.size()) {
-        size_t end = path.find('/', start);
-        if (end == std::string::npos) {
-            end = path.size();
-        }
+    int hdnode_private_ckd(HDNode *inout, uint32_t i);
 
-        std::string part = path.substr(start, end - start);
-        start = end + 1;
-
-        bool is_hardened = part.back() == '\'';
-        int index = std::stoi(part.substr(0, part.size() - (is_hardened ? 1 : 0)));
-        if (is_hardened) {
-            index += 0x80000000;
-        }
-
-        std::vector<unsigned char> index_bytes = {
-            static_cast<unsigned char>(index >> 24),
-            static_cast<unsigned char>(index >> 16),
-            static_cast<unsigned char>(index >> 8),
-            static_cast<unsigned char>(index)
-        };
-
-        std::vector<unsigned char> data = { 0x00 };
-        data.insert(data.end(), privkey.begin(), privkey.end());
-        data.insert(data.end(), index_bytes.begin(), index_bytes.end());
-
-        std::vector<unsigned char> I = hmac_sha512(chain_code, data);
-
-        privkey = std::vector<unsigned char>(I.begin(), I.begin() + 32);
-        chain_code = std::vector<unsigned char>(I.begin() + 32, I.end());
-    }
-
-    BIP32Node child;
-    child.privkey = privkey;
-    return child;
 }
 
 
-std::vector<unsigned char> private_to_public_key(const std::vector<unsigned char>& privkey) {
-    EC_KEY* ec_key = EC_KEY_new_by_curve_name(NID_secp256k1);
-    if (ec_key == nullptr) {
-        return {};
+std::string bytes_to_hex_string(const uint8_t* data, size_t len) {
+    std::stringstream ss;
+    for (size_t i = 0; i < len; i++) {
+        ss << std::setw(2) << std::setfill('0') << std::hex << (int)data[i];
     }
-
-    BIGNUM* priv_bn = BN_bin2bn(privkey.data(), privkey.size(), nullptr);
-    if (!EC_KEY_set_private_key(ec_key, priv_bn)) {
-        return {};
-    }
-
-    const EC_GROUP* group = EC_KEY_get0_group(ec_key);
-    EC_POINT* pub_point = EC_POINT_new(group);
-    if (!EC_POINT_mul(group, pub_point, priv_bn, nullptr, nullptr, nullptr)) {
-        return {};
-    }
-
-    unsigned char pubkey[33];
-    size_t pubkey_len = 0;
-    if (!EC_POINT_point2oct(group, pub_point, point_conversion_form_t::POINT_CONVERSION_COMPRESSED, pubkey, sizeof(pubkey), nullptr)) {
-        return {};
-    }
-    
-    pubkey_len = 33;
-    std::vector<unsigned char> pubkey_vec(pubkey, pubkey + pubkey_len);
-
-    EC_POINT_free(pub_point);
-    BN_free(priv_bn);
-    EC_KEY_free(ec_key);
-
-    return pubkey_vec;
+    return ss.str();
 }
 
+wallet generate_evm_wallet(std::string mnemonic){
 
-wallet generate_evm_wallet(const std::string& mnemonic) {
-    std::vector<unsigned char> seed = mnemonic_to_seed(mnemonic);
+    uint8_t seed[64];
+    HDNode node;
 
-    BIP32Node master_key = derive_master_key(seed);
-    std::string path = "44'/60'/0'/0/0";
-    BIP32Node child_key = derive_key(master_key, path);
+    mnemonic_to_seed(mnemonic.c_str(), "", seed, NULL);
+    hdnode_from_seed(seed, sizeof(seed), SECP256K1_NAME, &node);
 
-    std::string private_key = bytearray2hex(child_key.privkey);
-    std::string public_key = keccak256(private_key.substr(2));
-    std::string address = public_key.substr(public_key.size() - 40);
-    address = "0x" + address;
+    HDNode node_eth = node;
+    hdnode_private_ckd_prime(&node_eth, 44);
+    hdnode_private_ckd_prime(&node_eth, 60);
+    hdnode_private_ckd_prime(&node_eth, 0);
+    hdnode_private_ckd(&node_eth, 0);
+    hdnode_private_ckd(&node_eth, 0);
+    hdnode_fill_public_key(&node_eth);
 
+
+    uint8_t pub_key[65];
+    const ecdsa_curve *curve = &secp256k1;
+    ecdsa_uncompress_pubkey(curve, node_eth.public_key, pub_key);
+	
+    uint8_t keccak256_hash[32];
+    keccak_256(pub_key + 1, 64, keccak256_hash);
+    std::string eth_address = "0x" + bytes_to_hex_string(keccak256_hash + 12, 20);
+	
     wallet wal;
-    wal.address = address;
-    wal.public_key = public_key;
-    wal.private_key = private_key;
+    wal.address = eth_address;
+    wal.public_key = bytes_to_hex_string(node_eth.public_key, 33);
+    wal.private_key = bytes_to_hex_string(node_eth.private_key, 32);
     wal.mnemonic = mnemonic;
     return wal;
 }
 
-wallet generate_bitcoin_wallet(const std::string& mnemonic) {
-  
-    std::vector<unsigned char> seed = mnemonic_to_seed(mnemonic);
-    BIP32Node master_key = derive_master_key(seed);
-    std::string path = "84'/0'/0'/0/0";
-    BIP32Node child_key = derive_key(master_key, path);
 
-    std::vector<unsigned char> pubKey = private_to_public_key(child_key.privkey);
-    std::vector<unsigned char> sha256Hash = sha256(pubKey);
-    std::vector<unsigned char> ripemd160Hash = ripemd160(sha256Hash);
-    std::string address = bech32_encode(ripemd160Hash);
+
+wallet generate_bitcoin_wallet(const std::string mnemonic){
+
+    uint8_t seed[64];
+    HDNode node;
+
+    mnemonic_to_seed(mnemonic.c_str(), "", seed, NULL);
+    hdnode_from_seed(seed, sizeof(seed), SECP256K1_NAME, &node);
+	
+    HDNode node84 = node;
+    hdnode_private_ckd_prime(&node84, 84);
+    hdnode_private_ckd_prime(&node84, 0);
+    hdnode_private_ckd_prime(&node84, 0);
+    hdnode_private_ckd(&node84, 0);
+    hdnode_private_ckd(&node84, 0);
+    hdnode_fill_public_key(&node84);
+
+
+    uint8_t sha[32], hash160[20];
+    sha256_Raw(node84.public_key, 33, sha);
+    ripemd160(sha, 32, hash160); 
+
+    char address_bech32[100];
+    segwit_addr_encode(address_bech32, "bc", 0, hash160, 20);
+
+    HDNode node44 = node;
+    hdnode_private_ckd_prime(&node44, 44);
+    hdnode_private_ckd_prime(&node44, 0);
+    hdnode_private_ckd_prime(&node44, 0);
+    hdnode_private_ckd(&node44, 0);
+    hdnode_private_ckd(&node44, 0);
+    hdnode_fill_public_key(&node44);
+
+    sha256_Raw(node44.public_key, 33, sha);
+    ripemd160(sha, 32, hash160);
+
+    uint8_t legacy_addr[21];
+    legacy_addr[0] = 0x00;
+    memcpy(legacy_addr + 1, hash160, 20);
+
+    char address_legacy[64];
+    base58_encode_check(legacy_addr, 21, HASHER_SHA2D, address_legacy, sizeof(address_legacy));
+
+    HDNode node49 = node;
+    hdnode_private_ckd_prime(&node49, 49);
+    hdnode_private_ckd_prime(&node49, 0);
+    hdnode_private_ckd_prime(&node49, 0);
+    hdnode_private_ckd(&node49, 0);
+    hdnode_private_ckd(&node49, 0);
+    hdnode_fill_public_key(&node49);
+
+    sha256_Raw(node49.public_key, 33, sha);
+    ripemd160(sha, 32, hash160);
+
+ 
+    uint8_t redeem_script[22];
+    redeem_script[0] = 0x00; 
+    redeem_script[1] = 0x14;
+    memcpy(redeem_script + 2, hash160, 20);
+
+    sha256_Raw(redeem_script, 22, sha);
+    ripemd160(sha, 32, hash160);
+
+    uint8_t sh_addr[21];
+    sh_addr[0] = 0x05;
+    memcpy(sh_addr + 1, hash160, 20);
+
+    char address_p2sh[64];
+    base58_encode_check(sh_addr, 21, HASHER_SHA2D, address_p2sh, sizeof(address_p2sh));
+
 
     wallet wal;
-    wal.address = address;
-    wal.public_key = bytearray2hex(pubKey);
-    wal.private_key = bytearray2hex(child_key.privkey);
     wal.mnemonic = mnemonic;
+    wal.private_key = bytes_to_hex_string(node84.private_key, 32);
+    wal.public_key = bytes_to_hex_string(node84.public_key, 33);
+    wal.address = address_bech32;
+    wal.address_legacy = address_legacy;
+    wal.address_p2sh = address_p2sh;
     return wal;
 }
