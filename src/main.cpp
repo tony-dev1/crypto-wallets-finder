f89d88c95b031cee7cf76c46e620c9fb3990fdb1
TonyDevBTC <micheline_farrier@hotmail.com>
2025-05-22 11:59:20 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
+/* 
+* This file is part of the Trezor Crypto library.
+* 
+* Copyright (c) 2021 SatoshiLabs s.r.o. (https://www.satoshilabs.com)
+* 
+* Modified by TonyDev BTC for use in WalletGen.
+* 
+* This code is licensed under the MIT License (MIT), see LICENSE file for details.
+*/
+
+
+
 #include <iostream>
 #include <Windows.h>
-#include "bip39-words.hpp"
-#include "addres_generator.h"
-#include "address_checker.h"
 
+#include "address_checker.h"
+#include "address_generator.h"
+#include <limits>
+#include <iomanip>
+
+
+extern "C" {
+    const char *mnemonic_generate(int strength);
+}
 
 std::string save_found_path = "found_wallets.txt";
 
@@ -26,8 +44,11 @@ void print_logo() {
     std::cout << "*                  |_.__/ \\__, |   |_|\\___/|_| |_|\\__, |___|____/ \\__\\___|*\n";
     std::cout << "*                         |___/                   |___/_____|             *\n";
     std::cout << "***************************************************************************\n";
-    std::cout << "Github: https://github.com/tony-dev-btc/walletgen\n";
+    std::cout << "Github: https://github.com/tony-dev1/walletgen\n";
     std::cout << "\n";
+
+ 
+
 }
 
 void print_donate_info() {
@@ -43,31 +64,49 @@ void print_donate_info() {
 int show_menu() {
     set_color(9);
  point1:
-    std::cout << "============================================================================\n";
-    std::cout << "'1' - generate one BTC wallet                                              =\n";
-    std::cout << "'2' - generate one EVM wallet (ETH, BNB, MATIC e.t.c)                      =\n";
-    std::cout << "'3' - search BTC wallets with balance (using the Internet - slower)        =\n";
-    std::cout << "'4' - search BTC wallets with balance (using the database - faster)        =\n";
-    std::cout << "'5' - search EVM wallets with balance (using the Internet - slower)        =\n";
-    std::cout << "'6' - search EVM wallets with balance (using the database - faster)        =\n";
-    std::cout << "'7' - support the developer                                                =\n";
-    std::cout << "'8' - exit                                                                 =\n";
-    std::cout << "============================================================================\n";
+    std::cout << "\033[34m============================================================================\n";
+    std::cout << "\033[38;5;214m[1]\033[94m - generate one BTC wallet                                              \033[34m=\n";
+    std::cout << "\033[38;5;214m[2]\033[94m - generate one EVM wallet (ETH, BNB, MATIC e.t.c)                      \033[34m=\n";
+    std::cout << "\033[38;5;214m[3]\033[94m - search BTC wallets with balance (using the Internet - slower)        \033[34m=\n";
+    std::cout << "\033[38;5;214m[4]\033[94m - search BTC wallets with balance (using the database - faster)        \033[34m=\n";
+    std::cout << "\033[38;5;214m[5]\033[94m - search EVM wallets with balance (using the Internet - slower)        \033[34m=\n";
+    std::cout << "\033[38;5;214m[6]\033[94m - search EVM wallets with balance (using the database - faster)        \033[34m=\n";
+    std::cout << "\033[38;5;214m[7]\033[94m - support the developer                                                \033[34m=\n";
+    std::cout << "\033[38;5;214m[8]\033[94m - exit                                                                 \033[34m=\n";
+    std::cout << "\033[34m============================================================================\n";
     set_color(7);
 
     std::cout << "select an action using the key: ";
+
     int action;
-    std::cin >> action;
+    while(!(std::cin >> action)){
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin.clear();
+    }
     return action;
 }
 
 void print_wallet(wallet wal, int type) {
     set_color(8);
+
     std::cout << "-----------------------------------------------------------------------------\n";
-    std::cout << (type == 0 ? "BTC" : "EVM") << " address: " << wal.address << "\n";
-    std::cout << "public key:  " << wal.public_key << "\n";
-    std::cout << "private key: " << wal.private_key << "\n";
-    std::cout << "mnemonic:    " << wal.mnemonic << "\n";
+    if(type == WAL_TYPE_BTC){
+        std::cout << "[Bitcoin Wallet]\n";
+        std::cout << "BECH32 address:   " << wal.address << "\n";
+        std::cout << "P2SH address:     " << wal.address_p2sh << "\n";
+        std::cout << "P2PKH address:    " << wal.address_legacy << "\n";
+        std::cout << "public key:       " << wal.public_key << "\n";
+        std::cout << "private key:      " << wal.private_key << "\n";
+        std::cout << "mnemonic: " << wal.mnemonic << "\n";
+    }
+    else if(type == WAL_TYPE_EVM){
+        std::cout << "[EVM/Ethereum Wallet]\n";
+        std::cout << "address:     " << wal.address << "\n";
+        std::cout << "public key:  " << wal.public_key << "\n";
+        std::cout << "private key: " << wal.private_key << "\n";
+        std::cout << "mnemonic: " << wal.mnemonic << "\n";
+    }
+
     std::cout << "-----------------------------------------------------------------------------\n\n";
     set_color(7);
 }
@@ -100,18 +139,14 @@ void save_wallet_data(wallet wal, wallet_data wal_data, int type) {
 void start_search_btc_wallets(int way = 0);
 void start_search_evm_wallets(int way = 0);
 
+
+
 int main(int argc, char** argv) {
- 
+     
     walletgen_path = std::string(argv[0]);
     int n = walletgen_path.find_last_of("\\");
     if (n != std::string::npos)
         walletgen_path = walletgen_path.substr(0, n);
-
-    if (!load_bip39_words()) {
-        std::cout << "failed to load bip39 words from 'bip39-words.txt'.\n";
-        system("pause");
-        return 0;
-    }
 
     if (argc > 1) {
         if (strstr(argv[1], "gen-btc")) {
@@ -119,7 +154,7 @@ int main(int argc, char** argv) {
             if (argc > 2)
                 count = std::atoi(argv[2]);
             for (int i = 0; i < count; ++i) {
-                std::string mnemonic = generate_mnemonic(12);
+                std::string mnemonic = std::string(mnemonic_generate(12));
                 wallet wal = generate_bitcoin_wallet(mnemonic);
                 print_wallet(wal, 0);
             }
@@ -129,7 +164,7 @@ int main(int argc, char** argv) {
             if (argc > 2)
                 count = std::atoi(argv[2]);
             for (int i = 0; i < count; ++i) {
-                std::string mnemonic = generate_mnemonic(12);
+                std::string mnemonic = std::string(mnemonic_generate(12));
                 wallet wal = generate_evm_wallet(mnemonic);
                 print_wallet(wal, 0);
             }
@@ -147,7 +182,8 @@ int main(int argc, char** argv) {
         return 0;
     }
 
-    SetConsoleTitleA("[WalletGen] - Menu");
+    std::string title = "[WalletGen] - Menu";
+    SetConsoleTitleA(title.c_str());
     set_color(6);
     print_logo();
     set_color(7);
@@ -155,15 +191,20 @@ int main(int argc, char** argv) {
     while (true) {
 
         int action = show_menu();
+
+        generate_wallet_test();
+
         std::cout << "\n";
 
         if (action == 1) {
-            std::string mnemonic = generate_mnemonic(12);
+            std::string mnemonic = std::string(mnemonic_generate(128));
             wallet wal = generate_bitcoin_wallet(mnemonic);
             print_wallet(wal, 0);
         }
+
+
         else if (action == 2) {
-            std::string mnemonic = generate_mnemonic(12);
+            std::string mnemonic = std::string(mnemonic_generate(128));
             wallet wal = generate_evm_wallet(mnemonic);
             print_wallet(wal, 1);
         }
@@ -174,7 +215,7 @@ int main(int argc, char** argv) {
             start_search_btc_wallets(1);
         }
         else if (action == 5) {
-            start_search_evm_wallets(0);
+             start_search_evm_wallets(0);
         }
         else if (action == 6) {
             start_search_evm_wallets(1);
@@ -182,7 +223,7 @@ int main(int argc, char** argv) {
         else if (action == 7) {
             print_donate_info();
         }
-        else if (action == 8) {
+        else if (action == 8) { 
             break;
         }
         else {
@@ -190,6 +231,7 @@ int main(int argc, char** argv) {
         }
 
         std::cout << "\n\n";
+
     }
 
     return 0;
@@ -224,20 +266,22 @@ void start_search_btc_wallets(int way)
     unsigned long long checked_mnemonics = 0;
     float found_balance = 0.0f;
     int speed = 0, speed_check = 0, mnemonics_by_sec = 0;
-
+    std::cout << std::fixed << std::setprecision(4);
+ 
     while (true) {
-        std::string mnemonic = generate_mnemonic(12);
-        wallet wal = generate_bitcoin_wallet(mnemonic);
 
-        wallet_data wal_data = checker.get_wallet_data(wal.address, WAL_TYPE_BTC);
-        
-        std::cout << "mnemonic: " << wal.mnemonic << "\naddress:  " << wal.address << "\nbalance:  " << wal_data.btc_amount << " btc\n";
+        std::string mnemonic = std::string(mnemonic_generate(128));
+        wallet wal = generate_bitcoin_wallet(mnemonic);
+        wallet_data wal_data = checker.get_wallet_data(wal, WAL_TYPE_BTC);
+        std::cout << "\033[38;5;214mmnemonic:\033[0m  " << wal.mnemonic << "\n\033[38;5;214maddresses:\033[0m " << wal.address << " | " << wal.address_legacy << " | " << wal.address_p2sh
+        << "\n\033[38;5;214mbalance:\033[0m   " << wal_data.btc_amount << " btc\n";
+        std::cout << "\033[94m===============================================================================================================================\033[0m\n";
 
         if (wal_data.btc_amount > 0) {
             save_wallet_data(wal, wal_data, WAL_TYPE_BTC);
             found_balance += wal_data.btc_amount * coingecko_api::get_btc_udst_price();
 
-            std::string new_title = "[WalletGen] - Found BTC wallet / [Checked mnemonics: " + std::to_string(checked_mnemonics) + " | Found balance: " + std::to_string(found_balance) +
+            std::string new_title = "[WalletGen | " + std::string(TYPE) + "] - Found BTC wallet / [Checked mnemonics: " + std::to_string(checked_mnemonics) + " | Found balance: " + std::to_string(found_balance) +
                 "$ | Speed: " + std::to_string(speed) + "/s]";
             SetConsoleTitleA(new_title.c_str());
             print_wallet(wal, WAL_TYPE_BTC);
@@ -249,16 +293,20 @@ void start_search_btc_wallets(int way)
             speed_check = time(0);
         }
 
-        checked_mnemonics++;
-        mnemonics_by_sec++;
 
-        std::string new_title = "[WalletGen] - Seaching for BTC wallets / [Checked mnemonics: " + std::to_string(checked_mnemonics) + " | Found balance: " + std::to_string(found_balance) +
-            "$ | Speed: " + std::to_string(speed) + "/s]";
-        SetConsoleTitleA(new_title.c_str());
+        if(checked_mnemonics % 100 == 0){
+            std::string new_title = "[WalletGen | " + std::string(TYPE) + "] - Seaching for BTC wallets / [Checked mnemonics: " + std::to_string(checked_mnemonics) + " | Found balance: " + std::to_string(found_balance) + 
+                "$ | Speed: " + std::to_string(speed) + "/s]";
+            SetConsoleTitleA(new_title.c_str());
+            }
+
+            checked_mnemonics++;
+            mnemonics_by_sec++;
+
+        }
+
     }
    
-
-}
 
 void start_search_evm_wallets(int way)
 {
@@ -276,12 +324,12 @@ void start_search_evm_wallets(int way)
     else {
         if (!wallets_data_base::evm_database_exists()) {
             set_color(4);
-            std::cout << "[Error]\nFailed to load EVM wallets data base from '" << EVM_WAL_DATABASE_NAME << "'.\nDownload this file to https://github.com/tony-dev-btc/walletgen/releases/tag/database\nAnd move to " << walletgen_path << "\n";
+            std::cout << "[Error]\nFailed to load EVM wallets data base from '" << EVM_WAL_DATABASE_NAME << "'.\nDownload this file to https://github.com/tony-dev1/walletgen/releases/tag/database\n And move to " << walletgen_path << "\n";
             set_color(7);
             return;
         }
 
-        std::cout << "Loading the ETH database from " BTC_WAL_DATABASE_NAME << "...\n";
+        std::cout << "Loading the ETH database from " EVM_WAL_DATABASE_NAME << "...\n";
         wallets_data_base::load_evm_database();
     }
 
@@ -291,15 +339,15 @@ void start_search_evm_wallets(int way)
 
     while (true) {
 
-        std::string mnemonic = generate_mnemonic(12);
+        std::string mnemonic = std::string(mnemonic_generate(128));
         wallet wal = generate_evm_wallet(mnemonic);
-
-        wallet_data wal_data = checker.get_wallet_data(wal.address, WAL_TYPE_EVM);
-
-        std::cout << "mnemonic: " << wal.mnemonic << "\naddress:  " << wal.address << "\nbalance:  ";
+        wallet_data wal_data = checker.get_wallet_data(wal, WAL_TYPE_EVM);
+        wal = generate_evm_wallet(mnemonic, false);
+            wal_data = checker.get_wallet_data(wal, WAL_TYPE_EVM);
+        std::cout << "\033[38;5;214mmnemonic:\033[0m " << wal.mnemonic << "\n\033[38;5;214maddress:\033[0m  " << wal.address << "\n\033[38;5;214mbalance:\033[0m  ";
 
         if (wal_data.eth_amount == 0 && wal_data.bnb_amount == 0 && wal_data.matic_amount == 0) {
-            std::cout << "(empty)\n";
+         std::cout << "(empty)\n\033[94m==========================================================================================\033[0m\n";
         }
         else {
             std::cout << "(with balance)\n";
@@ -320,13 +368,15 @@ void start_search_evm_wallets(int way)
             speed_check = time(0);
         }
 
-        checked_mnemonics++;
-        mnemonics_by_sec++;
 
-        std::string new_title = "[WalletGen] - Seaching for EVM wallets / [Checked mnemonics: " + std::to_string(checked_mnemonics) + " | Found balance: " + std::to_string(found_balance) + 
+        if(checked_mnemonics % 100 == 0){
+        std::string new_title = "[WalletGen | " + std::string(TYPE) + "] - Seaching for EVM wallets / [Checked mnemonics: " + std::to_string(checked_mnemonics) + " | Found balance: " + std::to_string(found_balance) + 
             "$ | Speed: " + std::to_string(speed) + "/s]";
         SetConsoleTitleA(new_title.c_str());
 
+        }
+        checked_mnemonics++;
+        mnemonics_by_sec++;
     }
 
 }
